4500716efb1b9c7a0e033b2065af8a3ca0006681
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-11-19 12:03:12 -0600

    Se modificó el script para hacer push -> 19/11/2023 12:03:12

--- script.sh
+++ script.sh
@@ -3,16 +3,26 @@
 # Colors
 red='\e[1;31m'
 green='\e[1;32m'
+cyan='\e[1;34m'
 reset='\e[0m'
+
+# Get status for the local changes
+STATUS=$(git status | grep -io "nothing to commit")
+
+# If status message is "nothing to commit" the script ends
+if [ "${STATUS}" == "nothing to commit" ]; then
+    echo -e "${cyan}There's no changes${reset}"
+    exit 1
+fi
 
 # Add file .docx to working directory
 git add "INFORME FINAL RP-23.docx"
 
-# Commit message
+# Add a commit message
 echo -e -n "${green}Agrega un nombre de versión: ${reset}"
 read COMMIT_MESSAGE
 
-# If the commit message is empty end the script
+# If the commit message is empty the script ends
 if [ "${COMMIT_MESSAGE}" == "" ]; then
     echo -e "${red}You need to specify a message for this file version${reset}"
     echo -e "${red}Run the script again${reset}"
@@ -21,6 +31,7 @@ fi
 
 # Get the current date for the commit
 CURRENT_DATE=$(date +"%d/%m/%Y %H:%M:%S")
+echo "${COMMIT_MESSAGE} -> ${CURRENT_DATE}"
 
 # Make the commit
 git commit -m "${COMMIT_MESSAGE} -> ${CURRENT_DATE}"
