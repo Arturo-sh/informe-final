753d3d62c1aa4d6d6d141c0a23983c8d4b4a8bf2
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-11-19 12:28:15 -0600

    Se añadió al script la ruta del repositorio local -> 19/11/2023 12:28:15

--- script.sh
+++ script.sh
@@ -7,15 +7,18 @@ blue='\e[1;34m'
 cyan='\e[1;36m'
 reset='\e[0m'
 
+# Move to the repository path
+cd /c/xampp/htdocs/informe-final/
+
 # Get status for the local changes
 STATUS=$(git status | grep -io "nothing to commit")
 
 # If status message is "nothing to commit", terminate the script
 if [ "${STATUS}" == "nothing to commit" ]; then
     echo -e "${blue}There's no changes${reset}"
-    
-    # Wait for the user to press a key
-    echo -e -n "${cyan}Press a key to continue...${reset}" && read
+
+    # Wait for the user to press Enter key to continue
+    echo -e -n "${cyan}Press Enter key to continue...${reset}" && read
     exit 1
 fi
 
@@ -31,8 +34,8 @@ if [ "${COMMIT_MESSAGE}" == "" ]; then
     echo -e "${red}You need to specify a message for this file version${reset}"
     echo -e "${red}Run the script again${reset}"
 
-    # Wait for the user to press a key
-    echo -e -n "${cyan}Press a key to continue...${reset}" && read
+    # Wait for the user to press Enter key to continue
+    echo -e -n "${cyan}Press Enter key to continue...${reset}" && read
     exit 1
 fi
 
@@ -46,5 +49,5 @@ git commit -m "${COMMIT_MESSAGE} -> ${CURRENT_DATE}"
 # Push changes to remote repository
 git push
 
-# Wait for the user to press a key
-echo -e -n "${cyan}Press a key to continue...${reset}" && read
+# Wait for the user to press Enter key
+echo -e -n "${cyan}Press Enter key to continue...${reset}" && read
