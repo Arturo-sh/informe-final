e455f58d6ac3b94c3a491abe2e7f80b7eb0eaf9e
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-11-19 21:35:45 -0600

    Se modificó el script para commits -> 19/11/2023 21:35:45

--- script.sh
+++ script.sh
@@ -3,23 +3,51 @@
 # Colors
 red='\e[1;31m'
 green='\e[1;32m'
+yellow='\e[1;33m'
 blue='\e[1;34m'
+purple='\e[1;35m'
 cyan='\e[1;36m'
 reset='\e[0m'
 
+# Boxes for the messages
+box1=$(echo -e ${green}[${reset}${green}*${reset}${green}]${reset})
+box2=$(echo -e ${green}[${reset}${blue}*${reset}${green}]${reset})
+box3=$(echo -e ${green}[${reset}${red}*${reset}${green}]${reset})
+box4=$(echo -e ${green}[${reset}${purple}*${reset}${green}]${reset})
+
 # Move to the repository path
 cd /c/xampp/htdocs/informe-final/
+
+# Function to terminate the script
+function handleCommitStatus() {
+    case "${1}" in
+        "0")
+            echo -e "\v\t${box1} ${green}Commit successfully applied${reset}"
+            ;;
+        "1")
+            echo -e "\v\t${box2} ${blue}There are no changes to apply${reset}"
+            ;;
+        "2")
+            echo -e "\v\t${box3} ${red}You need to specify a message for the commit${reset}"
+            echo -e "\t    ${red}Press Ctrl + C to exit${reset}"
+            return
+            ;;
+        *)
+            echo -e "\v\t${box4} ${purple}Unknow error code${reset}"
+            ;;
+    esac
+
+    # Wait for the user to press Enter key to continue
+    echo -e -n "\v\t${cyan}Press Enter key to continue...${reset}" && read -r
+    exit ${1}
+}
 
 # Get status for the local changes
 STATUS=$(git status | grep -io "nothing to commit")
 
 # If status message is "nothing to commit", terminate the script
-if [ "${STATUS}" == "nothing to commit" ]; then
-    echo -e "${blue}There's no changes${reset}"
-
-    # Wait for the user to press Enter key to continue
-    echo -e -n "${cyan}Press Enter key to continue...${reset}" && read
-    exit 1
+if [[ "${STATUS}" == "nothing to commit" ]]; then
+    handleCommitStatus 1
 fi
 
 # Add file .docx to working directory
@@ -29,23 +57,21 @@ git add "INFORME FINAL RP-23.docx"
 git status
 echo -e "${blue}------------------------------${reset}"
 
-# Add a commit message
-echo -e -n "${green}Agregue un nombre de versión: ${reset}"
-read COMMIT_MESSAGE
+while true; do
+    # Add a commit message
+    echo -e -n "\n${green}Mensaje del commit: ${reset}"
+    read -r COMMIT_MESSAGE
 
-# If the commit message is empty, terminate the script
-if [ "${COMMIT_MESSAGE}" == "" ]; then
-    echo -e "${red}You need to specify a message for this file version${reset}"
-    echo -e "${red}Run the script again${reset}"
-
-    # Wait for the user to press Enter key to continue
-    echo -e -n "${cyan}Press Enter key to continue...${reset}" && read
-    exit 1
-fi
+    # If the commit message is empty, terminate the script
+    if [[ -z "${COMMIT_MESSAGE}" ]]; then
+        handleCommitStatus 2
+    else
+        break
+    fi
+done
 
 # Get the current date for the commit
 CURRENT_DATE=$(date +"%d/%m/%Y %H:%M:%S")
-echo "${COMMIT_MESSAGE} -> ${CURRENT_DATE}"
 
 # Make the commit
 git commit -m "${COMMIT_MESSAGE} -> ${CURRENT_DATE}"
@@ -53,5 +79,5 @@ git commit -m "${COMMIT_MESSAGE} -> ${CURRENT_DATE}"
 # Push changes to remote repository
 git push
 
-# Wait for the user to press Enter key
-echo -e -n "${cyan}Press Enter key to continue...${reset}" && read
+# Terminate the script with exit code successful (0)
+handleCommitStatus 0
